588b38a9ee8244f0e8d563bef80514b1a555174a
dqsully <djqballer@outlook.com>
2015-04-09 14:20:01 -0600

    fix icon menu scrolling on chrome

--- base.js
+++ base.js
@@ -54,6 +54,36 @@ document.addEventListener('click' ,function(e) {
     outclicks.push(tmp[i]);
   }
 });
+
+navigator.sayswho= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+
+navigator.browser= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    return M[1];
+})();
 // End Library
 
 
@@ -214,7 +244,12 @@ function menuUp(e) {
 
 function menuScroll(e) {
   if(ic.offsetHeight > icons.offsetHeight) {
-    sm += e.deltaY * 16;
+    // sm += e.deltaY * 16;
+    if(navigator.browser == 'Firefox') {
+      sm += e.deltaY * 16;
+    } else {
+      sm += e.deltaY;
+    }
     if(sm + icons.offsetHeight < ic.offsetHeight && sm > 0) {
       ic.style.marginTop = '-' + sm + 'px';
     } else if(sm + icons.offsetHeight >= ic.offsetHeight) {
